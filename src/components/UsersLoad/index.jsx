8b0df8a36e4f01537bc15b6dcb5f80edf076fc02
al-poberezhnyi <al.poberezhnyi@gmail.com>
2025-02-01 18:28:51 +0200

    initial branch dev

---
+++ src/components/UsersLoad/index.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class UsersLoad extends Component {
+    constructor (props) {
+        super(props);
+
+        this.state = {};
+    }
+    render () {
+        return <div>index</div>;
+    }
+}
+export default UsersLoad;
