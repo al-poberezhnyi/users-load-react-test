71d2e20b9e62eed378ba519c51c774e15939163c
al-poberezhnyi <al.poberezhnyi@gmail.com>
2025-02-04 19:45:44 +0200

    fix api

--- src/api/index.js
+++ src/api/index.js
@@ -3,7 +3,6 @@ import queryString from 'query-string';
 import CONSTANTS from '../constants';
 
 function loadUsers (options) {
-    console.log('options :>> ', options);
     const defaultOptions = {
         page: 1,
         results: 10,
@@ -12,12 +11,10 @@ function loadUsers (options) {
     };
 
     const realOptions = { ...defaultOptions, ...options };
-    console.log('realOptions :>> ', realOptions);
 
     const stringifiedOptions = queryString.stringify(realOptions, {
         arrayFormat: 'comma',
     });
-    console.log('stringifiedOptions :>> ', stringifiedOptions);
 
     return fetch(`${CONSTANTS.BASE_URL}?${stringifiedOptions}`).then(res =>
         res.json()
