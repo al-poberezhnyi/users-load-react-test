f389d094981eb2874b39e99120f5115d1d342db0
al-poberezhnyi <al.poberezhnyi@gmail.com>
2025-02-04 16:22:56 +0200

    add constants

---
+++ src/constants.js
@@ -0,0 +1,15 @@
+//
+export default {
+    BASE_URL: 'https://randomuser.me/api/',
+    INCLUDING_FIELDS: [
+        'name',
+        'gender',
+        'location',
+        'email',
+        'phone',
+        'login',
+        'picture',
+        'dob',
+    ],
+    SEED: 'alpo',
+};
